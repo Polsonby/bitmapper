23324aa7c1d967d34323a3f319839ea1b20ff2bd
Polsonby <ipanderson@gmail.com>
2015-06-29 23:56:29 +0100

    Test for ignoring row or column being drawn backwards was not testing draw_vertical

--- test/test.rb
+++ test/test.rb
@@ -66,6 +66,7 @@ describe "Grid" do
 		grid = Bitmapper.new
 		grid.create(3,3)
 		grid.draw_horizontal(3, 1, 1, "X")
+		grid.draw_vertical(1, 3, 1, "X")		
 		grid.show.must_equal ["OOO", "OOO", "OOO"]
 	end
 end
