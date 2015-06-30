4e58b9c3b7425c3c2ef0434cfea4fc8d041b8135
Polsonby <ipanderson@gmail.com>
2015-06-30 18:20:15 +0100

    Added fill to repl and added alternative lower case commands for usability

--- repl.rb
+++ repl.rb
@@ -5,7 +5,6 @@ class Runner
 	
 	def initialize
 		@bitmapper = Bitmapper.new
-
 		loop do
 		  print "> "
 		  handle_input(gets.chomp!)
@@ -15,19 +14,21 @@ class Runner
 	def handle_input(input)
 		command, *params = input.split /\s/
 		case command
-		when 'I'
+		when 'I', 'i'
 			@bitmapper.create(params[0], params[1])
-		when 'C'
+		when 'C', 'c'
 			@bitmapper.init_grid
-		when 'L'
+		when 'L', 'l'
 			@bitmapper.paint(params[0], params[1], params[2])
-		when 'H'
+		when 'H', 'h'
 			@bitmapper.draw_horizontal(params[0], params[1], params[2], params[3]) 
-		when 'V'
+		when 'V', 'v'
 			@bitmapper.draw_vertical(params[0], params[1], params[2], params[3]) 
-		when 'S'
+		when 'F', 'f'
+			@bitmapper.fill(params[0], params[1], params[2])
+		when 'S', 's'
 			puts @bitmapper.show
-		when 'X'
+		when 'X', 'x'
 			exit
 		end
 	end
